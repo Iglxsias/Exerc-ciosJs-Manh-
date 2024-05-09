cd23251fa70f220d104eefe5f41231ceeac503f6
ISABELA IGLESIAS <47265072812@info.local>
2024-05-09 08:43:16 -0300

    feat: exercicio 01

---
+++ ex1.js
@@ -0,0 +1,23 @@
+//Exercício 1: Verificação de login
+//Escreva um programa que solicita ao usuário um nome de usuário 
+//e uma senha. Verifique se o nome de usuário 
+//é "admin" e a senha é "senha123". 
+//Exiba uma mensagem indicando se o login foi bem-sucedido ou não.
+
+//solicita ao usuário um nome de usuário
+let username = prompt("Digite um nome de usuario: ")
+
+//solicita ao usuário uma senha.
+let senha = prompt("DIgite a senha: ")
+
+//Verifique se o nome de usuário é admin&quot; e a senha é &quot;senha123&quot;
+//CASO SIM -> Exiba a mensagem de login bem sucedida.
+//CASO NÃO -> Exiba a mensagem de usuário ou senha incorreta.
+
+if (username == "admin" && senha == "senha123"){
+    console.log("Login bem sucedido!")
+} 
+else{
+    console.log("Você não tem permissão de acesso!")
+}
+
---
+++ exemplo.js
@@ -0,0 +1,50 @@
+//Apresentar alguma mensagem na tela
+//alert("Hello Word!!!")
+
+//Apresenta uma mensagem no console
+//console.log("Hello Word");
+
+//Escreva um programa que solicite o nome do usuario
+// e lhe de boas vindas, exibindo uma mensagem pelo console.
+
+//Apresneta uma mensagem e pega alguma informação do usuário
+//prompt() -> recebe uma entrada digitada
+//let nome = prompt("Ola, digite o seu nome: ");
+
+//Concatenação -> juntar texto
+//console.log("Seja bem vindo " + nome + "!!!");
+
+//let pontos = 100;
+
+//parseInt() -> Transforma o que esta entre () em numero inteiro.
+//let bonus = parseInt(prompt ("Quantos voce deseja somar?"));
+
+//Adição -> num1 + num2
+//Subtração-> num1 - num2
+//Multiplicação -> num1 * num2
+//Divisão -> num1 / num2
+
+//let totalPontos = pontos + bonus
+
+//console.log(totalPontos)
+
+//let notaAluno = parseFloat(prompt("Digite a nota do aluno: "));
+//console.log(notaAluno)
+
+// let nota1 = parseInt(prompt("Digite uma nota: "))
+
+// let nota2 = parseInt(prompt("Digite outra nota: "))
+
+// let media = (nota1 + nota2)/2
+
+// console.log(media)
+
+
+//parseInt --> Transforma o que esta entre () em numero com virgula / numero frasionado
+let produto1 = parseFloat(prompt("Digite o valor do produto: "))
+
+let produto2 = parseFloat(prompt("Digite o valor do segundo produto: "))
+
+let soma = produto1 + produto2
+
+console.log(soma)
