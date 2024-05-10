23bd2781845d5a834679604f262758567403a2ba
ISABELA IGLESIAS <47265072812@info.local>
2024-05-10 08:30:40 -0300

    feat: exercício 06

---
+++ ex6.js
@@ -0,0 +1,15 @@
+// Exercício 6: Verificação de idade
+// Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
+// de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
+
+// solicita o ano de nascimento de uma pessoa
+let data = parseInt(prompt("Digite o ano em que nasceu: "))
+
+// verifica se ela é maior de 18 anos
+
+if (data >= 2006){
+    console.log("Maior de idade!")
+}
+else{
+    console.log("Não é maior de idade!")
+}
