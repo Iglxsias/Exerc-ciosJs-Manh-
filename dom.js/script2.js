8db6932b421b8617a65f2523b1255de51025690c
ISABELA IGLESIAS <47265072812@info.local>
2024-05-20 10:57:29 -0300

    feat: finalizando exemplo

---
+++ dom.js/script2.js
@@ -0,0 +1,39 @@
+let titulo = window.document.getElementById("titulo")
+
+titulo.style.color = "green"
+
+titulo.style.fontFamily = "Montserrat"
+
+//inicializa um variavel
+let clique = 0
+
+//funcao para contagem de cliques
+const somarCliques = () => {
+
+    //incrementou 1 valor da variavel
+    clique++
+
+    //seleciona o botao por querSelector e atualiza a quantudade de cliques no html
+    document.querySelector("#botao").innerHTML = clique
+
+}
+
+
+const adicionarItem = () => {
+    //criar um novo elemento li(item da lista)
+    let novoItem = document.createElement("li")
+
+    //define o texto do item
+    novoItem.textContent = "Novo Item"
+
+    //Identifica o elemento que vai ser adicionado
+    let lista = document.getElementById("minhaLista")
+
+    //Adiciona um novo item a lista
+    lista.appendChild(novoItem);
+
+}
+
+const voltar
+
+console.log(clique)
